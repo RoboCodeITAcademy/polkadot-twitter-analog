65385766d22f1d165eba837a24f837fd506dc49b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-21 15:13:49 +0500

    account settings

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    follower = models.PositiveIntegerField(default=0)
+    post = models.PositiveIntegerField(default=0)
+    rank = models.CharField(max_length=200, blank=True)
+    short_info = models.CharField(max_length=200, blank=True)
+
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserEditForm, ProfileEditForm
+from .models import Profile
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            Profile.objects.create(user=user)
+
+            return render(request, "app.html")
+    else:
+        form = UserRegisterForm()
+    return render(request, "account/register.html", {"form":form})
+
+@login_required(login_url="/account/login/")
+def profile(request):
+    return render(request, "account/detail.html")
+
+def edit(request,user_id):
+    if request.method == "POST":
+        user_edit_form = UserEditForm(request.POST)
+        profile_edit_form = ProfileEditForm(request.POST)
+        if user_edit_form.is_valid():
+            user_edit_form.save()
+            return redirect("/account/profile/")
+        if profile_edit_form.is_valid():
+            profile_edit_form.save()
+            return redirect("/account/profile/")
+    else:
+        user_edit_form = UserEditForm()
+        profile_edit_form = ProfileEditForm()
+
+    context = {
+        "user_form":user_edit_form,
+        "profile_form":profile_edit_form
+    }
+    return render(request, "account/edit.html", context)
